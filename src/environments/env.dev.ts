c96cfdcf7c5fb8b648229021c70029e3020aa959
fyambos <feliciteyambos@gmail.com>
2024-12-22 16:53:24 +0100

    tech: set up angular fire

---
+++ src/environments/env.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBoNtBaen3lbw4aYf707IjnW7YphhKr4Os",
+        authDomain: "ticket-app-115e1.firebaseapp.com",
+        projectId: "ticket-app-115e1",
+        storageBucket: "ticket-app-115e1.firebasestorage.app",
+        messagingSenderId: "315833123057",
+        appId: "1:315833123057:web:4222ce457657dc9a21a950",
+        measurementId: "G-Z1FVNWQTED"
+    }
+};
